9e51b168bd59043125c0518fbdfce0a7130784bb
Volodymyr <coremio3@gmail.com>
2022-12-03 23:28:04 +0100

    try connect with server in component element

--- client/src/components/add/elements/Elements.jsx
+++ client/src/components/add/elements/Elements.jsx
@@ -1,14 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./styleElements/elements.css";
+import { URL } from "../../../url/URL";
+
+const apiElement = axios.create({ baseURL: `${URL}/element` });
 
 const Elements = () => {
-  const handleAddSubmit = async (e) => {};
-  const handleChange = (e) => {};
-  // const handleChangeSelect = (e) => {
-  //   console.log("Fruit Selected!!");
+  const [element, setElement] = useState([]);
+  const [addElement, setAddElement] = useState({
+    element: "",
+    urlPicture: "",
+  });
 
-  //   // setNameProdukt(e.target.value);
-  // };
+  const fetchGET = async () => {
+    try {
+      // setLoading(true);
+      const res = await apiElement.get();
+      console.log(res.data);
+      setElement(res.data);
+      // setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAddSubmit = async (e) => {
+    const newElement = {
+      element: addElement.element,
+      urlPicture: addElement.urlPicture,
+    };
+    apiElement
+      .post("/", newElement)
+      .then((response) => {
+        fetchGET();
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    setAddElement("");
+  };
+  const handleChange = (e) => {
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    fetchGET();
+  }, []);
+
+  const handlGetElement = (data) => {
+    return data.map((item, index) => {
+      return (
+        <div className="div-get" key={index}>
+          <span>{item.id}</span>
+          <span>{item.element}</span>
+          <span>{item.urlPicture}</span>
+        </div>
+      );
+    });
+  };
+
   return (
     <div>
       <form onSubmit={handleAddSubmit}>
@@ -23,6 +75,7 @@ const Elements = () => {
 
         <button type="submit">add</button>
       </form>
+      <div className="div-get">{handlGetElement(element)}</div>
     </div>
   );
 };
